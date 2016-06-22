7ca81fdeb27f44e4fe4830f5d4b66bb4eafe74d0
Otto Urpelainen <oturpe@iki.fi>
2016-06-22 09:42:10 +0200

    Initial sequencing for four devices

---
+++ timer_program.ino
@@ -0,0 +1,59 @@
+// Not working (?)
+//    d8
+#define WATER_LIGHTS 12
+#define PARFANJE 2
+#define DRESS 4
+#define FOG 7
+
+#define SEQUENCE_LENGTH 20
+// { seconds, pin, state }
+int sequence[SEQUENCE_LENGTH][3] = {
+    { 3 * 60 + 44, WATER_LIGHTS, HIGH },
+    { 4 * 60 + 57, DRESS, HIGH },
+    { 6 * 60 + 45, WATER_LIGHTS, LOW },
+    { 6 * 60 + 51, DRESS, LOW },
+    { 12 * 60 + 46, PARFANJE, HIGH },
+    { 13 * 60 + 58, PARFANJE, LOW },
+    { 15 * 60 + 30, WATER_LIGHTS, HIGH },
+    { 16 * 60 + 4, PARFANJE, HIGH },
+    { 17 * 60 + 48, WATER_LIGHTS, LOW },
+    { 17 * 60 + 48, PARFANJE, LOW },
+    { 27 * 60 + 42, FOG, HIGH },
+    { 31 * 60 + 35, FOG, LOW },
+    { 31 * 60 + 46, DRESS, HIGH },
+    { 33 * 60 + 05, PARFANJE, HIGH },
+    { 33 * 60 + 34, WATER_LIGHTS, HIGH },
+    { 34 * 60 + 53, WATER_LIGHTS, LOW },
+    { 34 * 60 + 53, PARFANJE, LOW },
+    { 36 * 60 + 30, DRESS, LOW }
+};
+
+void setup(){
+    pinMode(WATER_LIGHTS, OUTPUT);
+    pinMode(PARFANJE, OUTPUT);
+    pinMode(DRESS, OUTPUT);
+    pinMode(FOG, OUTPUT);
+    
+    digitalWrite(WATER_LIGHTS, LOW);
+    digitalWrite(PARFANJE, LOW);
+    digitalWrite(DRESS, LOW);
+    digitalWrite(FOG, LOW);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+
+int counter = 0;
+int seconds = 0;
+void loop(){
+    int * step = sequence[counter];
+    
+    while(seconds < step[0]) {
+        delay(1000);
+        seconds++;
+    }
+
+    digitalWrite(step[1],step[2]);
+    counter++;
+}
+
